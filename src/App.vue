33f5eb30e3e4c6670c6ea26388a25792f8dd8986
Ali Kaafarani <ali@kvikshaug.no>
2019-04-23 09:18:44 +0200

    feat: add navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,92 @@
         ></v-toolbar-side-icon>
         <v-toolbar-title>Caffeine</v-toolbar-title>
       </v-toolbar>
+
+      <v-navigation-drawer v-model="drawer" app clipped>
+        <v-list>
+          <v-list-tile :to="'/'">
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile :to="'/interactiveMap'">
+            <v-list-tile-action>
+              <!-- TODO: use custom icon -->
+              <v-icon></v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Interactive Map</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile :to="'/design'">
+            <v-list-tile-action>
+              <v-icon>edit</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Design</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile :to="'/jobs'">
+            <v-list-tile-action>
+              <v-icon>hourglass_empty</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Jobs</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile :to="'/designs'">
+            <v-list-tile-action>
+              <v-icon>device_hub</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Designs</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile :to="'/projects'">
+            <v-list-tile-action>
+              <v-icon>subject</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Projects</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile :to="'/maps'">
+            <v-list-tile-action>
+              <v-icon>map</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Maps</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile :to="'/models'">
+            <v-list-tile-action>
+              <v-icon>rounded_corner</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Models</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+
       <v-content>
         <v-container fluid>
           <router-view />
@@ -16,3 +102,13 @@
     </v-app>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    drawer: true
+  })
+});
+</script>
