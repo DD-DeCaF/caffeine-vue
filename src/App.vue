59ea7a293062920b707bcaac9c3553422f5bf212
Ali Kaafarani <ali@kvikshaug.no>
2019-04-23 09:12:00 +0200

    feat: add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div id="app">
-    <router-view />
+    <v-app>
+      <v-toolbar app clipped-left color="primary" dark>
+        <v-toolbar-side-icon
+          @click.stop="drawer = !drawer"
+        ></v-toolbar-side-icon>
+        <v-toolbar-title>Caffeine</v-toolbar-title>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid>
+          <router-view />
+        </v-container>
+      </v-content>
+      <v-footer app></v-footer>
+    </v-app>
   </div>
 </template>
