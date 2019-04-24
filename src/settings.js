24ea88d1cac92819b09d35c0842066b46a9bc5ac
ChristianLieven <clie@biosustain.dtu.dk>
2019-04-24 14:39:07 +0200

    chore: define api endpoint for metabolicNinja

--- src/settings.js
+++ src/settings.js
@@ -4,7 +4,8 @@ export default {
     modelStorage: "https://api-staging.dd-decaf.eu/mstorage",
     warehouse: "https://api-staging.dd-decaf.eu/warehouse",
     maps: "https://api-staging.dd-decaf.eu/maps",
-    iam: "https://api-staging.dd-decaf.eu/iam"
+    iam: "https://api-staging.dd-decaf.eu/iam",
+    metabolicNinja: "https://api-staging.dd-decaf.eu/pathways-caffeine",
   },
   trustedURLs: ["https://api-staging.dd-decaf.eu", "http://localhost:4200"]
 };
