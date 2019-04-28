96d081171926b1db40f7d855da1774a154f0b531
Ali Kaafarani <ali@kvikshaug.no>
2019-04-28 16:23:48 +0200

    test: add mock store

--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -1,10 +1,35 @@
 import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Vuex from "vuex";
 import App from "@/App.vue";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    fetchDataError: null,
+    postDataError: null,
+    isDialogVisible: {
+      loader: false
+    },
+    loadingMessages: {
+      default: "Loading. Please wait."
+    },
+    session: {
+      isAuthenticated: false
+    }
+  },
+  actions: {
+    "session/interceptRequests": function() {},
+    "session/refreshTokenLoop": function() {},
+    fetchAllData: function() {}
+  }
+});
 
 describe("App.vue", () => {
   it("mounts successfully", () => {
-    const wrapper = shallowMount(App);
+    const wrapper = shallowMount(App, { store, localVue });
     expect(wrapper.text()).to.include("Caffeine");
   });
 });
