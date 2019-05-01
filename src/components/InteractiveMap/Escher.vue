3d79f1193b98c7e408a4987d98cd33ff04b64377
Ali Kaafarani <ali@kvikshaug.no>
2019-05-01 11:25:04 +0200

    refactor: move escher to a separate component

---
+++ src/components/InteractiveMap/Escher.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="escher-container fill-height">
+    <v-container fluid fill-height v-if="isInitializingEscher">
+      <v-layout align-center justify-center>
+        <p class="display-1">
+          <v-progress-circular
+            indeterminate
+            size="60"
+            :width="2"
+            class="mr-2"
+          ></v-progress-circular>
+          Initializing Escher, please wait...
+        </p>
+      </v-layout>
+    </v-container>
+    <div ref="escher" class="fill-height"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+/// <reference path="@/types/escher.d.ts" />
+import * as escher from "@dd-decaf/escher";
+
+export default Vue.extend({
+  name: "Escher",
+  props: ["mapData", "fluxDistribution"],
+  data: () => ({
+    escherBuilder: null,
+    isInitializingEscher: true
+  }),
+  watch: {
+    mapData(value) {
+      // Note that this will freeze the entire application, including
+      // progress spinners.
+      this.escherBuilder.load_map(value);
+      this.$emit("map-loaded");
+    },
+    fluxDistribution(value) {
+      this.escherBuilder.set_reaction_data(value);
+      this.escherBuilder._update_data(true, true);
+    }
+  },
+  methods: {
+    reactionState(id: string, type?: string) {
+      // TODO
+    },
+    handleKnockout(reactionId: string) {
+      // TODO
+    },
+    handleKnockoutGenes(reactionId: string) {
+      // TODO
+    },
+    handleSetAsObjective(reactionId: string) {
+      // TODO
+    },
+    handleChangeBounds(reactionId: string, lower: string, upper: string) {
+      // TODO
+    },
+    handleResetBounds(reactionId: string) {
+      // TODO
+    },
+    handleObjectiveDirection(reactionId: string) {
+      // TODO
+    }
+  },
+  mounted() {
+    this.escherBuilder = escher.Builder(null, null, null, this.$refs.escher, {
+      menu: "zoom",
+      scroll_behavior: "zoom",
+      fill_screen: false,
+      ignore_bootstrap: true,
+      never_ask_before_quit: true,
+      reaction_styles: ["color", "size", "text", "abs"],
+      identifiers_on_map: "bigg_id",
+      hide_all_labels: false,
+      hide_secondary_metabolites: false,
+      highlight_missing: true,
+      reaction_scale: [
+        { type: "min", color: "#A841D0", size: 20 },
+        { type: "Q1", color: "#868BB2", size: 20 },
+        { type: "Q3", color: "#6DBFB0", size: 20 },
+        { type: "max", color: "#54B151", size: 20 }
+      ],
+      reaction_no_data_color: "#CBCBCB",
+      reaction_no_data_size: 10,
+      tooltip: "custom",
+      enable_editing: false,
+      enable_fva_opacity: true,
+      show_gene_reaction_rules: true,
+      zoom_extent_canvas: true,
+      first_load_callback: () => {
+        this.isInitializingEscher = false;
+        this.$emit("escher-loaded");
+      },
+      reaction_state: this.reactionState,
+      tooltip_callbacks: {
+        knockout: this.handleKnockout,
+        knockoutGenes: this.handleKnockoutGenes,
+        setAsObjective: this.handleSetAsObjective,
+        changeBounds: this.handleChangeBounds,
+        resetBounds: this.handleResetBounds,
+        objectiveDirection: this.handleObjectiveDirection
+      }
+    });
+  }
+});
+</script>
--- src/components/InteractiveMap/InteractiveMap.vue
+++ src/components/InteractiveMap/InteractiveMap.vue
@@ -6,22 +6,12 @@
       class="my-0"
       height="12"
     ></v-progress-linear>
-    <div class="escher-container fill-height">
-      <v-container fluid fill-height v-if="isInitializingEscher">
-        <v-layout align-center justify-center>
-          <p class="display-1">
-            <v-progress-circular
-              indeterminate
-              size="60"
-              :width="2"
-              class="mr-2"
-            ></v-progress-circular>
-            Initializing Escher, please wait...
-          </p>
-        </v-layout>
-      </v-container>
-      <div ref="escher" class="fill-height"></div>
-    </div>
+    <Escher
+      @escher-loaded="escherLoaded"
+      @map-loaded="mapLoaded"
+      :fluxDistribution="fluxDistribution"
+      :mapData="mapData"
+    />
     <v-navigation-drawer permanent right absolute>
       <v-container class="py-1">
         <!-- TODO: Grouped maps -->
@@ -64,38 +54,47 @@
 <script lang="ts">
 import Vue from "vue";
 import axios from "axios";
-/// <reference path="@/types/escher.d.ts" />
-import * as escher from "@dd-decaf/escher";
 import * as settings from "@/settings";
+import Escher from "@/components/InteractiveMap/Escher.vue";
 import Card from "@/components/InteractiveMap/Card.vue";
 
 export default Vue.extend({
   name: "InteractiveMap",
   components: {
+    Escher,
     Card
   },
   data: () => ({
     escherBuilder: null,
-    isInitializingEscher: true,
     currentMapId: null,
     isLoadingMap: false,
+    fluxDistribution: null,
+    mapData: null,
     cards: []
   }),
   methods: {
+    escherLoaded() {
+      // TODO: Data might not be available at this point - need to latch onto
+      // the fetch action promise
+      const organism = this.$store.state.organisms.organisms[0];
+      const model = this.$store.state.models.models[2];
+      this.addCard("Design", organism, model, "pfba");
+    },
     changeMap() {
       this.isLoadingMap = true;
       // TODO: Get map from maps state lazy loader
       axios
         .get(`${settings.apis.maps}/maps/${this.currentMapId}`)
         .then(response => {
-          // Note that this will freeze the entire application, including
-          // progress spinners.
-          this.escherBuilder.load_map(response.data.map);
-          this.isLoadingMap = false;
+          this.mapData = response.data.map;
         })
         .catch(error => {
           // TODO: show snackbar
+          this.isLoadingMap = false;
         });
+    },
+    mapLoaded() {
+      this.isLoadingMap = false;
     },
     addCard(name, organism, model, method) {
       const card = {
@@ -118,88 +117,25 @@ export default Vue.extend({
         })
         .then(response => {
           card.growthRate = response.data.growth_rate;
-          this.escherBuilder.set_reaction_data(response.data.flux_distribution);
-          this.escherBuilder._update_data(true, true);
+          // TODO: Flux distribution should belong to the card
+          this.fluxDistribution = response.data.flux_distribution;
         })
         .catch(error => {
           // TODO: show snackbar
-          this.escherBuilder.set_reaction_data(null);
+          card.growthRate = null;
+          // TODO: Flux distribution should belong to the card
+          this.fluxDistribution = null;
           console.error(error);
         })
         .then(response => {
           card.isSimulating = false;
         });
-    },
-    reactionState(id: string, type?: string) {
-      // TODO
-    },
-    handleKnockout(reactionId: string) {
-      // TODO
-    },
-    handleKnockoutGenes(reactionId: string) {
-      // TODO
-    },
-    handleSetAsObjective(reactionId: string) {
-      // TODO
-    },
-    handleChangeBounds(reactionId: string, lower: string, upper: string) {
-      // TODO
-    },
-    handleResetBounds(reactionId: string) {
-      // TODO
-    },
-    handleObjectiveDirection(reactionId: string) {
-      // TODO
     }
   },
   computed: {
     maps() {
       return this.$store.state.maps.maps;
     }
-  },
-  mounted() {
-    this.escherBuilder = escher.Builder(null, null, null, this.$refs.escher, {
-      menu: "zoom",
-      scroll_behavior: "zoom",
-      fill_screen: false,
-      ignore_bootstrap: true,
-      never_ask_before_quit: true,
-      reaction_styles: ["color", "size", "text", "abs"],
-      identifiers_on_map: "bigg_id",
-      hide_all_labels: false,
-      hide_secondary_metabolites: false,
-      highlight_missing: true,
-      reaction_scale: [
-        { type: "min", color: "#A841D0", size: 20 },
-        { type: "Q1", color: "#868BB2", size: 20 },
-        { type: "Q3", color: "#6DBFB0", size: 20 },
-        { type: "max", color: "#54B151", size: 20 }
-      ],
-      reaction_no_data_color: "#CBCBCB",
-      reaction_no_data_size: 10,
-      tooltip: "custom",
-      enable_editing: false,
-      enable_fva_opacity: true,
-      show_gene_reaction_rules: true,
-      zoom_extent_canvas: true,
-      first_load_callback: () => {
-        this.isInitializingEscher = false;
-        // TODO: Data might not be available at this point - need to latch onto
-        // the fetch action promise
-        const organism = this.$store.state.organisms.organisms[0];
-        const model = this.$store.state.models.models[2];
-        this.addCard("Design", organism, model, "pfba");
-      },
-      reaction_state: this.reactionState,
-      tooltip_callbacks: {
-        knockout: this.handleKnockout,
-        knockoutGenes: this.handleKnockoutGenes,
-        setAsObjective: this.handleSetAsObjective,
-        changeBounds: this.handleChangeBounds,
-        resetBounds: this.handleResetBounds,
-        objectiveDirection: this.handleObjectiveDirection
-      }
-    });
   }
 });
 </script>
