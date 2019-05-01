4d94c047531b07c54774454cf8b8d260aeeb80f5
ChristianLieven <clie@biosustain.dtu.dk>
2019-05-01 08:27:10 +0200

    feat: implement editing on models view

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -1,13 +1,8 @@
 <template>
   <v-container>
-    <EditMap
-      v-model="isMapEditDialogVisible"
-      :mapItem="selectedMapItem"
-      :mapItemIndex="maps.indexOf(selectedMapItem)"
-    />
     <DeletionDialog
       v-model="isDeletionDialogVisible"
-      :items="[selectedMapItem]"
+      :items="[mapItem]"
       itemsType="maps"
       @toggleLoader="toggleLoader()"
     />
@@ -78,20 +73,135 @@
         </v-tooltip>
       </v-flex>
     </v-layout>
+    <!-- Definition of the edit dialog -->
+     <v-dialog v-model="isMapEditDialogVisible" width="650">
+      <v-card class="pa-2">
+        <v-container grid-list-lg text-md-left>
+          <v-layout fill-height column wrap>
+            <v-flex md6>
+              <h3>Edit {{ mapItem.name }}</h3>
+            </v-flex>
+            <v-flex>
+              <v-form
+                ref="form"
+                v-model="valid"
+                @keyup.native.enter="editMap"
+              >
+                <v-text-field
+                  required
+                  v-model="mapItem.name"
+                  :rules="[rules.required]"
+                  name="name"
+                  label="Name"
+                  type="text"
+                  placeholder="e.g. My Favourite Map"
+                ></v-text-field>
+                <v-autocomplete
+                  required
+                  item-text="name"
+                  item-value="id"
+                  v-model="mapItem.project_id"
+                  :items="availableProjects"
+                  :rules="[rules.required]"
+                  name="project"
+                  label="Project"
+                  type="text"
+                >
+                  <template v-slot:append-item>
+                    <v-divider class="my-2"></v-divider>
+                    <!-- Work out why clicking on the project creation dialog will close it. How do I mimik the behaviour of the old platform here? -->
+                    <v-btn
+                      color="secondary"
+                      @click="$store.commit('toggleDialog', 'project')"
+                    >
+                      <v-icon>add</v-icon>
+                      New project
+                    </v-btn>
+                  </template>
+                </v-autocomplete>
+                <v-autocomplete
+                  required
+                  item-text="name"
+                  item-value="id"
+                  v-model="mapItem.model_id"
+                  :items="availableModels"
+                  :rules="[rules.required]"
+                  persistent-hint
+                  name="model"
+                  label="Model"
+                  type="text"
+                >
+                  <template v-slot:append-item>
+                    <v-divider class="my-2"></v-divider>
+                    <v-btn
+                      color="secondary"
+                      @click="$store.commit('toggleDialog', 'model')"
+                    >
+                      <v-icon>add</v-icon>
+                      New Model
+                    </v-btn>
+                  </template>
+                </v-autocomplete>
+              </v-form>
+            </v-flex>
+          </v-layout>
+        </v-container>
+
+        <v-divider class="my-2"></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="secondary"
+            flat
+            @click.stop="isMapEditDialogVisible = false"
+            :disabled="$store.state.isDialogVisible.loader"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="primary"
+            @click="editMap"
+            :disabled="$store.state.isDialogVisible.loader || !valid"
+          >
+            Edit
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- Definition of notifications and banners -->
+    <v-snackbar
+      color="success"
+      v-model="isMapEditSuccess"
+      bottom
+      :timeout="3000"
+    >
+      {{ mapItem.name }} successfully edited.
+    </v-snackbar>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { mapGetters } from 'vuex';
 import axios from "axios";
 import { AxiosResponse } from "axios";
 import settings from "@/settings";
+import { mapGetters } from 'vuex';
 
 export default Vue.extend({
   name: "Maps",
   data: () => ({
-    selectedMapItem: {id: null, name: null, model_id: null, project_id: null},
+    valid: false,
+    isMapEditSuccess: false,
+    isInvalidCredentials: false,
+    isUnauthorized: false,
+    isNotFound: false,
+    hasOtherError: false,
+    rules: {
+      required: value => !!value || "Required."
+    },
+    mapItem: {id: null, name: null, model_id: null, project_id: null},
+    mapItemIndex: null,
     isMapEditDialogVisible: false,
     isDeletionDialogVisible: false,
     headers: [
@@ -115,12 +225,36 @@ export default Vue.extend({
   }),
   methods: {
     editItem(item) {
-      this.selectedMapItem = item;
+      this.mapItem = item;
       this.isMapEditDialogVisible = true
     },
      deleteItem(item) {
-      this.selectedMapItem = item;
+      this.mapItem = item;
       this.isDeletionDialogVisible = true
+    },
+    editMap() {
+      axios
+      .put(`${settings.apis.maps}/maps/${this.mapItem.id}`, this.mapItem)
+      .then((response: AxiosResponse) => {
+        this.$store.commit("toggleDialog", "loader");
+        this.$store.commit("maps/editMap", this.mapItem, this.mapItemIndex);
+        this.isMapEditSuccess = true;
+        this.isMapEditDialogVisible = false;
+      })
+      .catch(error => {
+          if (error.response && error.response.status === 401) {
+            this.isInvalidCredentials = true;
+          } else if (error.response && error.response.status === 403) {
+            this.isUnauthorized = true;
+          } else if (error.response && error.response.status === 404) {
+            this.isNotFound = true;
+          } else {
+            this.hasOtherError = true;
+          }
+      })
+      .then(() => {
+          this.$store.commit("toggleDialog", "loader");
+        });
     }
   },
   computed: {
@@ -132,7 +266,13 @@ export default Vue.extend({
     },
     ...mapGetters({
       model: "models/getModelById"
-    })
+    }),
+  availableProjects() {
+      return this.$store.state.projects.projects;
+    },
+  availableModels() {
+      return this.$store.state.models.models;
+    }
   }
 });
 </script>
--- src/components/Models.vue
+++ src/components/Models.vue
@@ -1,5 +1,11 @@
 <template>
   <v-container>
+        <DeletionDialog
+      v-model="isDeletionDialogVisible"
+      :items="[modelItem]"
+      itemsType="models"
+      @toggleLoader="toggleLoader()"
+    />
     <v-layout justify-center>
       <v-flex md6>
         <h1>Models</h1>
@@ -66,15 +72,147 @@
         </v-tooltip>
       </v-flex>
     </v-layout>
+     <!-- Definition of the edit dialog -->
+     <v-dialog v-model="isModelEditDialogVisible" width="650">
+      <v-card class="pa-2">
+        <v-container grid-list-lg text-md-left>
+          <v-layout fill-height column wrap>
+            <v-flex md6>
+              <h3>Edit {{ modelItem.name }}</h3>
+            </v-flex>
+            <v-flex>
+              <v-form
+                ref="form"
+                v-model="valid"
+                @keyup.native.enter="editModel"
+              >
+                <v-text-field
+                  required
+                  v-model="modelItem.name"
+                  :rules="[rules.required]"
+                  name="name"
+                  label="Name"
+                  type="text"
+                  placeholder="e.g. My Favourite Model"
+                ></v-text-field>
+                <v-autocomplete
+                  required
+                  item-text="name"
+                  item-value="id"
+                  v-model="modelItem.organism_id"
+                  :items="availableOrganisms"
+                  :rules="[rules.required]"
+                  name="organism"
+                  label="Organism"
+                  type="text"
+                >
+                  <template v-slot:append-item>
+                    <v-divider class="my-2"></v-divider>
+                    <v-btn
+                      color="secondary"
+                      @click="$store.commit('toggleDialog', 'organism')"
+                    >
+                      <v-icon>add</v-icon>
+                      New organism
+                    </v-btn>
+                  </template>
+                </v-autocomplete>
+                <v-autocomplete
+                  item-text="name"
+                  item-value="id"
+                  v-model="modelItem.map_id"
+                  :items="availableMaps"
+                  persistent-hint
+                  name="map"
+                  label="Preferred Map"
+                  type="text"
+                >
+                  <template v-slot:append-item>
+                    <v-divider class="my-2"></v-divider>
+                    <v-btn
+                      color="secondary"
+                      @click="$store.commit('toggleDialog', 'map')"
+                    >
+                      <v-icon>add</v-icon>
+                      New Map
+                    </v-btn>
+                  </template>
+                </v-autocomplete>
+                <v-autocomplete
+                  required
+                  item-text="name"
+                  item-value="id"
+                  v-model="modelItem.default_biomass_reaction"
+                  :items="availableReactions"
+                  hint="The reaction identifier of this model's default biomass reaction"
+                  persistent-hint
+                  name="biomass"
+                  label="Default Biomass Reaction"
+                  type="text"
+                >
+                </v-autocomplete>
+              </v-form>
+            </v-flex>
+          </v-layout>
+        </v-container>
+
+        <v-divider class="my-2"></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="secondary"
+            flat
+            @click.stop="isModelEditDialogVisible = false"
+            :disabled="$store.state.isDialogVisible.loader"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="primary"
+            @click="editModel"
+            :disabled="$store.state.isDialogVisible.loader || !valid"
+          >
+            Edit
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- Definition of notifications and banners -->
+    <v-snackbar
+      color="success"
+      v-model="isMapEditSuccess"
+      bottom
+      :timeout="3000"
+    >
+      {{ modelItem.name }} successfully edited.
+    </v-snackbar>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import axios from "axios";
+import { AxiosResponse } from "axios";
+import settings from "@/settings";
+import { mapGetters } from 'vuex';
 
 export default Vue.extend({
   name: "Models",
   data: () => ({
+    modelItemIndex: null,
+    isModelEditDialogVisible: false,
+    isDeletionDialogVisible: false,
+    valid: true,
+    isMapEditSuccess: false,
+    isInvalidCredentials: false,
+    isUnauthorized: false,
+    isNotFound: false,
+    hasOtherError: false,
+    rules: {
+      required: value => !!value || "Required."
+    },
+    modelItem: {id: null, name: null, map_id: null, project_id: null, organism_id: null},
     headers: [
           {
             text: 'Name',
@@ -95,6 +233,38 @@ export default Vue.extend({
     }
   }),
   methods: {
+    editItem(item) {
+      this.modelItem = item;
+      this.isModelEditDialogVisible = true
+    },
+     deleteItem(item) {
+      this.modelItem = item;
+      this.isDeletionDialogVisible = true
+    },
+    editModel() {
+      axios
+      .put(`${settings.apis.models}/models/${this.modelItem.id}`, this.modelItem)
+      .then((response: AxiosResponse) => {
+        this.$store.commit("toggleDialog", "loader");
+        this.$store.commit("models/editModel", this.modelItem, this.modelItemIndex);
+        this.isModelEditSuccess = true;
+        this.isModelEditDialogVisible = false;
+      })
+      .catch(error => {
+          if (error.response && error.response.status === 401) {
+            this.isInvalidCredentials = true;
+          } else if (error.response && error.response.status === 403) {
+            this.isUnauthorized = true;
+          } else if (error.response && error.response.status === 404) {
+            this.isNotFound = true;
+          } else {
+            this.hasOtherError = true;
+          }
+      })
+      .then(() => {
+          this.$store.commit("toggleDialog", "loader");
+        });
+    }
   },
   computed: {
     isAuthenticated() {
@@ -102,6 +272,23 @@ export default Vue.extend({
     },
     models() {
       return this.$store.state.models.models
+    },
+    ...mapGetters({
+      map: "maps/getMapById",
+      project: "projects/getProjectById",
+      organism: "organisms/getOrganismsById"
+    }),
+  availableProjects() {
+      return this.$store.state.projects.projects;
+    },
+  availableMaps() {
+      return this.$store.state.maps.maps;
+    },
+  availableOrganisms() {
+      return this.$store.state.organisms.organisms;
+    },
+    availableReactions() {
+      return ["Biomass1", "Biomass2"];
     },
   }
 });
