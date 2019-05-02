1eb0c101845716f17aea770bc954276a54fc8252
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2019-05-02 18:00:05 +0200

    feat: flesh out submission form

--- src/components/Design.vue
+++ src/components/Design.vue
@@ -3,7 +3,37 @@
 </template>
 
 <script lang="ts">
-export default {
-  name: "Design"
-};
+  export default {
+    name: "Design",
+    data: () => {
+      return {
+        species: "",
+        product: "",
+        project: "",
+        bigg: "",
+        rhea: "",
+        model: "",
+        maxPredictions: 3,
+        aerobic: false,
+        isSubmitted: false
+      };
+    },
+    methods: {
+      onSubmit() {
+        // trigger jobs polling
+        this.$router.push({ name: "jobs" });
+      }
+    },
+    validations: {},
+    beforeRouteLeave(to, from, next) {
+      if (!this.isSubmitted) {
+        const answer = window.confirm('Do you really want to leave? You have pending changes!')
+        if (answer) {
+          next();
+        } else {
+          next(false);
+        }
+      }
+    }
+  };
 </script>
