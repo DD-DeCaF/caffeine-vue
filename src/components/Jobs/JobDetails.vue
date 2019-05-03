ac27cdfef1accbcaf2fd100b744c2fe8f5d6a130
Anna Sirunian <anna_sirunian@ukr.net>
2019-05-03 14:45:42 +0200

    feat: add header

--- src/components/Jobs/JobDetails.vue
+++ src/components/Jobs/JobDetails.vue
@@ -1,6 +1,66 @@
-<template
-  ><p></p
-></template>
+<template>
+  <div>
+    <template v-if="job">
+      <v-container>
+        <v-layout column>
+          <v-flex>
+            <v-card>
+              <v-layout align-center justify-end row>
+                <h2 class="ma-3">
+                  <router-link to="/jobs" class="link">Jobs</router-link> / #{{
+                    jobId
+                  }}
+                </h2>
+                <v-spacer></v-spacer>
+                <v-list-tile class="ma-3">
+                  <v-icon class="mr-2">bug_report</v-icon>
+                  <div class="header-item">
+                    Organism:<br /><strong>{{ organism.name }}</strong>
+                  </div>
+                </v-list-tile>
+                <v-list-tile class="ma-3">
+                  <v-icon class="mr-2">rounded_corner</v-icon>
+                  <div class="header-item">
+                    Model:<br /><strong>{{ model.name }}</strong>
+                  </div>
+                </v-list-tile>
+                <v-list-tile class="ma-3">
+                  <v-icon class="mr-2">attach_money</v-icon>
+                  <div class="header-item">
+                    Product:<br /><strong>{{ job.product_name }}</strong>
+                  </div>
+                </v-list-tile>
+                <v-list-tile class="ma-3">
+                  <v-icon class="mr-2">timelapse</v-icon>
+                  <div class="header-item">
+                    Started:<br /><strong>{{
+                      job.created | moment("D MMM YYYY, HH:mm")
+                    }}</strong>
+                  </div>
+                </v-list-tile>
+                <v-list-tile class="ma-3">
+                  <v-icon class="mr-2">timelapse</v-icon>
+                  <div class="header-item">
+                    Completed:<br /><strong
+                      v-if="
+                        job.status !== 'STARTED' && job.status !== 'PENDING'
+                      "
+                      >{{ job.updated | moment("D MMM YYYY, HH:mm") }}</strong
+                    >
+                    <strong v-else>Still running</strong>
+                  </div>
+                </v-list-tile>
+              </v-layout>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </template>
+    <template v-else>
+      <v-progress-linear :indeterminate="true" class="my-0"></v-progress-linear>
+    </template>
+  </div>
+</template>
 
 <script lang="ts">
 import Vue from "vue";
@@ -9,9 +69,29 @@ import { mapGetters } from "vuex";
 export default Vue.extend({
   name: "JobDetails",
   computed: {
-    ...mapGetters({
-      job: "jobs/getJobById",
-    })
+    job() {
+      return this.$store.getters["jobs/getJobById"](this.jobId);
+    },
+    model() {
+      return this.$store.getters["models/getModelById"](this.job.model_id);
+    },
+    organism() {
+      return this.$store.getters["organisms/getOrganismById"](
+        this.job.organism_id
+      );
+    }
+  },
+  created() {
+    this.jobId = parseInt(this.$route.params.id);
   }
 });
 </script>
+
+<style scoped>
+.link {
+  text-decoration: none;
+}
+.header-item {
+  font-size: 14px;
+}
+</style>
