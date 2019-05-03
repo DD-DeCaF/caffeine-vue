ad966edb3ab58e4759db66d13fc793fa10434d87
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2019-05-03 15:05:20 +0200

    refactor: move privacy and tos to nav drawer
    Also lot's of changes due to running prettier for the first time.

--- src/App.vue
+++ src/App.vue
@@ -62,128 +62,131 @@
         </template>
       </v-toolbar>
       <v-navigation-drawer v-model="drawer" app clipped>
-        <v-list>
-          <v-list-tile to="/">
-            <v-list-tile-action>
-              <v-icon>home</v-icon>
-            </v-list-tile-action>
-
-            <v-list-tile-content>
-              <v-list-tile-title>Home</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-
-          <v-list-tile to="/interactiveMap">
-            <v-list-tile-action>
-              <v-icon>$vuetify.icons.interactive_map</v-icon>
-            </v-list-tile-action>
-
-            <v-list-tile-content>
-              <v-list-tile-title>Interactive Map</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-
-          <v-tooltip bottom :disabled="isAuthenticated">
-            <v-list-tile
-              slot="activator"
-              to="/design"
-              :disabled="!isAuthenticated"
-            >
+        <v-layout column fill-height>
+          <v-list>
+            <v-list-tile to="/">
               <v-list-tile-action>
-                <v-icon>edit</v-icon>
+                <v-icon>home</v-icon>
               </v-list-tile-action>
 
               <v-list-tile-content>
-                <v-list-tile-title>Design</v-list-tile-title>
+                <v-list-tile-title>Home</v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
-            <span> {{ disabledTooltipText }} </span>
-          </v-tooltip>
 
-          <v-tooltip bottom :disabled="isAuthenticated">
-            <v-list-tile
-              to="/jobs"
-              slot="activator"
-              :disabled="!isAuthenticated"
-            >
+            <v-list-tile to="/interactiveMap">
               <v-list-tile-action>
-                <v-icon>hourglass_empty</v-icon>
+                <v-icon>$vuetify.icons.interactive_map</v-icon>
               </v-list-tile-action>
 
               <v-list-tile-content>
-                <v-list-tile-title>Jobs</v-list-tile-title>
+                <v-list-tile-title>Interactive Map</v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
-            <span> {{ disabledTooltipText }} </span>
-          </v-tooltip>
 
-          <v-tooltip bottom :disabled="isAuthenticated">
-            <v-list-tile
-              to="/designs"
-              slot="activator"
-              :disabled="!isAuthenticated"
-            >
+            <v-tooltip bottom :disabled="isAuthenticated">
+              <v-list-tile
+                slot="activator"
+                to="/design"
+                :disabled="!isAuthenticated"
+              >
+                <v-list-tile-action>
+                  <v-icon>edit</v-icon>
+                </v-list-tile-action>
+
+                <v-list-tile-content>
+                  <v-list-tile-title>Design</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <span> {{ disabledTooltipText }} </span>
+            </v-tooltip>
+
+            <v-tooltip bottom :disabled="isAuthenticated">
+              <v-list-tile
+                to="/jobs"
+                slot="activator"
+                :disabled="!isAuthenticated"
+              >
+                <v-list-tile-action>
+                  <v-icon>hourglass_empty</v-icon>
+                </v-list-tile-action>
+
+                <v-list-tile-content>
+                  <v-list-tile-title>Jobs</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <span> {{ disabledTooltipText }} </span>
+            </v-tooltip>
+
+            <v-tooltip bottom :disabled="isAuthenticated">
+              <v-list-tile
+                to="/designs"
+                slot="activator"
+                :disabled="!isAuthenticated"
+              >
+                <v-list-tile-action>
+                  <v-icon>device_hub</v-icon>
+                </v-list-tile-action>
+
+                <v-list-tile-content>
+                  <v-list-tile-title>Designs</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <span> {{ disabledTooltipText }} </span>
+            </v-tooltip>
+
+            <v-tooltip bottom :disabled="isAuthenticated">
+              <v-list-tile
+                to="/projects"
+                slot="activator"
+                :disabled="!isAuthenticated"
+              >
+                <v-list-tile-action>
+                  <v-icon>subject</v-icon>
+                </v-list-tile-action>
+
+                <v-list-tile-content>
+                  <v-list-tile-title>Projects</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <span> {{ disabledTooltipText }} </span>
+            </v-tooltip>
+
+            <v-list-tile to="/maps">
               <v-list-tile-action>
-                <v-icon>device_hub</v-icon>
+                <v-icon>map</v-icon>
               </v-list-tile-action>
 
               <v-list-tile-content>
-                <v-list-tile-title>Designs</v-list-tile-title>
+                <v-list-tile-title>Maps</v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
-            <span> {{ disabledTooltipText }} </span>
-          </v-tooltip>
 
-          <v-tooltip bottom :disabled="isAuthenticated">
-            <v-list-tile
-              to="/projects"
-              slot="activator"
-              :disabled="!isAuthenticated"
-            >
+            <v-list-tile to="/models">
               <v-list-tile-action>
-                <v-icon>subject</v-icon>
+                <v-icon>rounded_corner</v-icon>
               </v-list-tile-action>
 
               <v-list-tile-content>
-                <v-list-tile-title>Projects</v-list-tile-title>
+                <v-list-tile-title>Models</v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
-            <span> {{ disabledTooltipText }} </span>
-          </v-tooltip>
-
-          <v-list-tile to="/maps">
-            <v-list-tile-action>
-              <v-icon>map</v-icon>
-            </v-list-tile-action>
-
-            <v-list-tile-content>
-              <v-list-tile-title>Maps</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-
-          <v-list-tile to="/models">
-            <v-list-tile-action>
-              <v-icon>rounded_corner</v-icon>
-            </v-list-tile-action>
-
-            <v-list-tile-content>
-              <v-list-tile-title>Models</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
+          </v-list>
+          <v-spacer></v-spacer>
+          <div>
+            <v-btn primary flat small to="/terms-of-service"
+              >Terms of Service</v-btn
+            >
+            <v-btn primary flat small to="/privacy-policy"
+              >Privacy Policy</v-btn
+            >
+          </div>
+        </v-layout>
       </v-navigation-drawer>
 
       <v-content>
         <router-view />
       </v-content>
-      <v-footer app justify-center dark color="primary" height="auto">
-        <v-spacer></v-spacer>
-        <div>
-          <v-btn primary flat small to="/terms-of-service">Terms of Service</v-btn>
-          <v-btn primary flat small to="/privacy-policy">Privacy Policy</v-btn>
-        </div>
-        <v-spacer></v-spacer>
-      </v-footer>
 
       <v-snackbar color="error" v-model="hasFetchDataError" :timeout="6000">
         Sorry, we were unable to retrieve some data from the server. Please try
--- src/components/PrivacyPolicy.vue
+++ src/components/PrivacyPolicy.vue
@@ -4,48 +4,154 @@
       <v-flex md6>
         <h1 id="privacy">Privacy Policy</h1>
 
-        <h3>Your privacy is critically important to us.</h3>Technical University of Denmark is located at:
-        <br>
+        <h3>Your privacy is critically important to us.</h3>
+        Technical University of Denmark is located at:
+        <br />
         <address>
           Technical University of Denmark
-          <br>DTU Biosustain Building 220, Kemitorvet, Lyngby
-          <br>2800 - Denmark , Denmark
-          <br>21798922
+          <br />DTU Biosustain Building 220, Kemitorvet, Lyngby <br />2800 -
+          Denmark , Denmark <br />21798922
         </address>
 
         <p>
-          It is Technical University of Denmark's policy to respect your privacy regarding any information we may collect while operating our website. This Privacy Policy applies to
-          <a href="https://caffeine.dd-decaf.eu">https://caffeine.dd-decaf.eu</a> (hereinafter, "us", "we", or "https://caffeine.dd-decaf.eu"). We respect your privacy and are committed to protecting personally identifiable information you may provide us through the Website. We have adopted this privacy policy ("Privacy Policy") to explain what information may be collected on our Website, how we use this information, and under what circumstances we may disclose the information to third parties. This Privacy Policy applies only to information we collect through the Website and does not apply to our collection of information from other sources.
+          It is Technical University of Denmark's policy to respect your privacy
+          regarding any information we may collect while operating our website.
+          This Privacy Policy applies to
+          <a href="https://caffeine.dd-decaf.eu"
+            >https://caffeine.dd-decaf.eu</a
+          >
+          (hereinafter, "us", "we", or "https://caffeine.dd-decaf.eu"). We
+          respect your privacy and are committed to protecting personally
+          identifiable information you may provide us through the Website. We
+          have adopted this privacy policy ("Privacy Policy") to explain what
+          information may be collected on our Website, how we use this
+          information, and under what circumstances we may disclose the
+          information to third parties. This Privacy Policy applies only to
+          information we collect through the Website and does not apply to our
+          collection of information from other sources.
         </p>
-        <p>This Privacy Policy, together with the Terms and conditions posted on our Website, set forth the general rules and policies governing your use of our Website. Depending on your activities when visiting our Website, you may be required to agree to additional terms and conditions.</p>
+        <p>
+          This Privacy Policy, together with the Terms and conditions posted on
+          our Website, set forth the general rules and policies governing your
+          use of our Website. Depending on your activities when visiting our
+          Website, you may be required to agree to additional terms and
+          conditions.
+        </p>
 
         <h2>Website Visitors</h2>
-        <p>Like most website operators, Technical University of Denmark collects non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request. Technical University of Denmark's purpose in collecting non-personally identifying information is to better understand how Technical University of Denmark's visitors use its website. From time to time, Technical University of Denmark may release non-personally-identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its website.</p>
-        <p>Technical University of Denmark also collects potentially personally-identifying information like Internet Protocol (IP) addresses for logged in users and for users leaving comments on https://caffeine.dd-decaf.eu blog posts. Technical University of Denmark only discloses logged in user and commenter IP addresses under the same circumstances that it uses and discloses personally-identifying information as described below.</p>
+        <p>
+          Like most website operators, Technical University of Denmark collects
+          non-personally-identifying information of the sort that web browsers
+          and servers typically make available, such as the browser type,
+          language preference, referring site, and the date and time of each
+          visitor request. Technical University of Denmark's purpose in
+          collecting non-personally identifying information is to better
+          understand how Technical University of Denmark's visitors use its
+          website. From time to time, Technical University of Denmark may
+          release non-personally-identifying information in the aggregate, e.g.,
+          by publishing a report on trends in the usage of its website.
+        </p>
+        <p>
+          Technical University of Denmark also collects potentially
+          personally-identifying information like Internet Protocol (IP)
+          addresses for logged in users and for users leaving comments on
+          https://caffeine.dd-decaf.eu blog posts. Technical University of
+          Denmark only discloses logged in user and commenter IP addresses under
+          the same circumstances that it uses and discloses
+          personally-identifying information as described below.
+        </p>
 
         <h2>Gathering of Personally-Identifying Information</h2>
-        <p>Certain visitors to Technical University of Denmark's websites choose to interact with Technical University of Denmark in ways that require Technical University of Denmark to gather personally-identifying information. The amount and type of information that Technical University of Denmark gathers depends on the nature of the interaction. For example, we ask visitors who sign up for a blog at https://caffeine.dd-decaf.eu to provide a username and email address.</p>
+        <p>
+          Certain visitors to Technical University of Denmark's websites choose
+          to interact with Technical University of Denmark in ways that require
+          Technical University of Denmark to gather personally-identifying
+          information. The amount and type of information that Technical
+          University of Denmark gathers depends on the nature of the
+          interaction. For example, we ask visitors who sign up for a blog at
+          https://caffeine.dd-decaf.eu to provide a username and email address.
+        </p>
 
         <h2>Security</h2>
-        <p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+        <p>
+          The security of your Personal Information is important to us, but
+          remember that no method of transmission over the Internet, or method
+          of electronic storage is 100% secure. While we strive to use
+          commercially acceptable means to protect your Personal Information, we
+          cannot guarantee its absolute security.
+        </p>
 
         <h2>Advertisements</h2>
-        <p>Ads appearing on our website may be delivered to users by advertising partners, who may set cookies. These cookies allow the ad server to recognize your computer each time they send you an online advertisement to compile information about you or others who use your computer. This information allows ad networks to, among other things, deliver targeted advertisements that they believe will be of most interest to you. This Privacy Policy covers the use of cookies by Technical University of Denmark and does not cover the use of cookies by any advertisers.</p>
+        <p>
+          Ads appearing on our website may be delivered to users by advertising
+          partners, who may set cookies. These cookies allow the ad server to
+          recognize your computer each time they send you an online
+          advertisement to compile information about you or others who use your
+          computer. This information allows ad networks to, among other things,
+          deliver targeted advertisements that they believe will be of most
+          interest to you. This Privacy Policy covers the use of cookies by
+          Technical University of Denmark and does not cover the use of cookies
+          by any advertisers.
+        </p>
 
         <h2>Links To External Sites</h2>
-        <p>Our Service may contain links to external sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy and terms and conditions of every site you visit.</p>
-        <p>We have no control over, and assume no responsibility for the content, privacy policies or practices of any third party sites, products or services.</p>
+        <p>
+          Our Service may contain links to external sites that are not operated
+          by us. If you click on a third party link, you will be directed to
+          that third party's site. We strongly advise you to review the Privacy
+          Policy and terms and conditions of every site you visit.
+        </p>
+        <p>
+          We have no control over, and assume no responsibility for the content,
+          privacy policies or practices of any third party sites, products or
+          services.
+        </p>
 
         <h2>Aggregated Statistics</h2>
-        <p>Technical University of Denmark may collect statistics about the behavior of visitors to its website. Technical University of Denmark may display this information publicly or provide it to others. However, Technical University of Denmark does not disclose your personally-identifying information.</p>
+        <p>
+          Technical University of Denmark may collect statistics about the
+          behavior of visitors to its website. Technical University of Denmark
+          may display this information publicly or provide it to others.
+          However, Technical University of Denmark does not disclose your
+          personally-identifying information.
+        </p>
 
         <h2>Cookies</h2>
-        <p>To enrich and perfect your online experience, Technical University of Denmark uses "Cookies", similar technologies and services provided by others to display personalized content, appropriate advertising and store your preferences on your computer.</p>
-        <p>A cookie is a string of information that a website stores on a visitor's computer, and that the visitor's browser provides to the website each time the visitor returns. Technical University of Denmark uses cookies to help Technical University of Denmark identify and track visitors, their usage of https://caffeine.dd-decaf.eu, and their website access preferences. Technical University of Denmark visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using Technical University of Denmark's websites, with the drawback that certain features of Technical University of Denmark's websites may not function properly without the aid of cookies.</p>
-        <p>By continuing to navigate our website without changing your cookie settings, you hereby acknowledge and agree to Technical University of Denmark's use of cookies.</p>
+        <p>
+          To enrich and perfect your online experience, Technical University of
+          Denmark uses "Cookies", similar technologies and services provided by
+          others to display personalized content, appropriate advertising and
+          store your preferences on your computer.
+        </p>
+        <p>
+          A cookie is a string of information that a website stores on a
+          visitor's computer, and that the visitor's browser provides to the
+          website each time the visitor returns. Technical University of Denmark
+          uses cookies to help Technical University of Denmark identify and
+          track visitors, their usage of https://caffeine.dd-decaf.eu, and their
+          website access preferences. Technical University of Denmark visitors
+          who do not wish to have cookies placed on their computers should set
+          their browsers to refuse cookies before using Technical University of
+          Denmark's websites, with the drawback that certain features of
+          Technical University of Denmark's websites may not function properly
+          without the aid of cookies.
+        </p>
+        <p>
+          By continuing to navigate our website without changing your cookie
+          settings, you hereby acknowledge and agree to Technical University of
+          Denmark's use of cookies.
+        </p>
 
         <h2>Privacy Policy Changes</h2>
-        <p>Although most changes are likely to be minor, Technical University of Denmark may change its Privacy Policy from time to time, and in Technical University of Denmark's sole discretion. Technical University of Denmark encourages visitors to frequently check this page for any changes to its Privacy Policy. Your continued use of this site after any change in this Privacy Policy will constitute your acceptance of such change.</p>
+        <p>
+          Although most changes are likely to be minor, Technical University of
+          Denmark may change its Privacy Policy from time to time, and in
+          Technical University of Denmark's sole discretion. Technical
+          University of Denmark encourages visitors to frequently check this
+          page for any changes to its Privacy Policy. Your continued use of this
+          site after any change in this Privacy Policy will constitute your
+          acceptance of such change.
+        </p>
 
         <h2></h2>
         <p></p>
@@ -58,8 +164,9 @@
             href="https://termsandconditionstemplate.com/privacy-policy-generator/"
             title="Privacy policy template generator"
             target="_blank"
-          >termsandconditionstemplate.com</a>. If you have any questions about this Privacy Policy, please contact us via
-          <a href="mailto:niso@biosustain.dtu.dk">email</a> or
+            >termsandconditionstemplate.com</a
+          >. If you have any questions about this Privacy Policy, please contact
+          us via <a href="mailto:niso@biosustain.dtu.dk">email</a> or
           <a href="tel:21798922">phone</a>.
         </p>
       </v-flex>
