1eb24dfd7118e107486d795ed7a75ee1b804fa9f
Ali Kaafarani <ali@kvikshaug.no>
2019-05-05 10:58:58 +0200

    refactor: move card dialog to separate component

--- src/components/InteractiveMap/Card.vue
+++ src/components/InteractiveMap/Card.vue
@@ -10,86 +10,11 @@
       <v-toolbar-title class="body-2">{{ card.name }}</v-toolbar-title>
       <v-spacer></v-spacer>
 
-      <v-dialog v-model="dialog" width="1200">
-        <template v-slot:activator="{ on }">
-          <v-btn flat icon v-on="on">
-            <v-icon color="white">edit</v-icon>
-          </v-btn>
-        </template>
-
-        <v-card class="pa-2">
-          <v-form>
-            <v-container>
-              <p class="headline">
-                Modify simulation card
-              </p>
-
-              <v-layout wrap>
-                <v-flex xs12 md3>
-                  <v-text-field
-                    label="Card name"
-                    v-model="card.name"
-                    required
-                  ></v-text-field>
-                </v-flex>
-                <v-flex xs12 md3>
-                  <v-select
-                    label="Organism"
-                    :items="organisms"
-                    v-model="card.organism"
-                    item-text="name"
-                    item-value="id"
-                    :hint="modificationsHint"
-                    persistent-hint
-                    return-object
-                    @change="onOrganismChange"
-                  ></v-select>
-                </v-flex>
-                <v-flex xs12 md3>
-                  <v-select
-                    label="Model"
-                    :items="modelsByOrganism"
-                    v-model="card.model"
-                    item-text="name"
-                    item-value="id"
-                    :hint="modificationsHint"
-                    persistent-hint
-                    return-object
-                  ></v-select>
-                </v-flex>
-                <v-flex xs12 md3>
-                  <v-select
-                    label="Method"
-                    :items="methods"
-                    v-model="card.method"
-                    item-text="name"
-                    item-value="id"
-                  ></v-select>
-                </v-flex>
-              </v-layout>
-
-              <v-data-table
-                hide-actions
-                :headers="modificationsHeaders"
-                :items="modifications"
-              >
-                <template v-slot:items="props">
-                  <td>{{ props.item.type }}</td>
-                  <td>{{ props.item.name }}</td>
-                  <td>{{ props.item.details }}</td>
-                </template>
-              </v-data-table>
-            </v-container>
-          </v-form>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" @click="simulateCard">
-              Simulate
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+      <CardDialog
+        :card="card"
+        :modifications="modifications"
+        @simulate-card="simulateCard"
+      />
 
       <v-btn flat icon v-if="!isOnlyCard" @click="removeCard">
         <v-icon color="white">close</v-icon>
@@ -165,11 +90,14 @@
 <script lang="ts">
 import Vue from "vue";
 import axios from "axios";
+import CardDialog from "@/components/InteractiveMap/CardDialog.vue";
 
 export default Vue.extend({
   name: "Card",
+  components: {
+    CardDialog
+  },
   data: () => ({
-    dialog: false,
     methods: [
       { id: "fba", name: "Flux Balance Analysis (FBA)" },
       { id: "pfba", name: "Parsimonious FBA" },
@@ -207,14 +135,6 @@ export default Vue.extend({
         return "grey";
       }
     },
-    organisms() {
-      return this.$store.state.organisms.organisms;
-    },
-    modelsByOrganism() {
-      return this.$store.state.models.models.filter(model => {
-        return model.organism_id === this.card.organism.id;
-      });
-    },
     modifications() {
       // Concatenate all modifications for a single table display.
       // TODO: Added reactions
@@ -240,15 +160,6 @@ export default Vue.extend({
         ...geneKnockouts,
         ...editedBounds
       ];
-    },
-    modificationsHint() {
-      if (this.modifications.length > 0) {
-        return `Changing this will reset ${
-          this.modifications.length
-        } modifications`;
-      } else {
-        return null;
-      }
     }
   },
   methods: {
@@ -260,13 +171,6 @@ export default Vue.extend({
     },
     simulateCard() {
       this.$emit("simulate-card", this.card);
-      this.dialog = false;
-    },
-    onOrganismChange() {
-      // When selected organism is updated, update the selected model
-      // correspondingly.
-      // TODO: Choose a default preferred model.
-      this.card.model = null;
     }
   }
 });
