80a8c34e9352d22937d550d3a83b97ba7f9637ba
Anna Sirunian <anna_sirunian@ukr.net>
2019-05-09 15:47:31 +0200

    feat: add sliders

--- src/components/Jobs/JobResultsTable.vue
+++ src/components/Jobs/JobResultsTable.vue
@@ -1,26 +1,26 @@
 <template>
   <v-card>
     <div>
-      <v-list class="table-buttons">
+      <v-list>
         <v-list-tile>
           <h3>Results</h3>
-          <v-layout justify-end>
-            <v-btn
-              flat
-              color="primary"
-              :disabled="selected.length < 1"
-              class="mt-3"
-              ><v-icon>share</v-icon>VISUALIZE</v-btn
-            >
-          </v-layout>
+          <v-spacer></v-spacer>
+          <v-btn
+            flat
+            color="primary"
+            :disabled="selected.length < 1"
+            class="mt-3"
+            ><v-icon>share</v-icon>VISUALIZE</v-btn
+          >
         </v-list-tile>
       </v-list>
       <v-data-table
         v-model="selected"
         :headers="headers"
-        :items="pathways"
+        :items="filteredPathways"
         :expand="expand"
         :pagination.sync="pagination"
+        :search="search"
         select-all
       >
         <template v-slot:headers="props">
@@ -34,18 +34,192 @@
                 @click.stop="toggleAll"
               ></v-checkbox>
             </th>
+
             <th
-              v-for="header in props.headers"
-              :key="header.text"
               :class="[
                 'column sortable',
                 pagination.descending ? 'desc' : 'asc',
-                header.value === pagination.sortBy ? 'active' : ''
+                'manipulation' === pagination.sortBy ? 'active' : ''
               ]"
-              @click="changeSort(header.value)"
+              @click="changeSort('manipulation')"
             >
-              <v-icon small>arrow_upward</v-icon>
-              {{ header.text }}
+              <v-icon>arrow_upward</v-icon>
+              Manipulations
+              <v-range-slider
+                @click.stop
+                v-model="filters.manipulations"
+                :min="range.manipulations[0]"
+                :max="range.manipulations[1]"
+                step="0"
+                thumb-label
+                always-dirty
+                thumb-size="24"
+                color="primary"
+                class="mt-4"
+              ></v-range-slider>
+            </th>
+
+            <th
+              :class="[
+                'column sortable',
+                pagination.descending ? 'desc' : 'asc',
+                'heterologous_reactions' === pagination.sortBy ? 'active' : ''
+              ]"
+              @click="changeSort('heterologous_reactions')"
+            >
+              <v-icon>arrow_upward</v-icon>
+              Heterologous reactions
+              <v-range-slider
+                @click.stop
+                v-model="filters.heterologous_reactions"
+                :min="range.heterologous_reactions[0]"
+                :max="range.heterologous_reactions[1]"
+                step="0"
+                thumb-label
+                thumb-size="24"
+                always-dirty
+                color="primary"
+                class="mt-4"
+              ></v-range-slider>
+            </th>
+
+            <th
+              :class="[
+                'column sortable',
+                pagination.descending ? 'desc' : 'asc',
+                'knockouts' === pagination.sortBy ? 'active' : ''
+              ]"
+              @click="changeSort('knockouts')"
+            >
+              <v-icon>arrow_upward</v-icon>
+              Knockouts
+              <v-range-slider
+                @click.stop
+                @change="filters"
+                v-model="filters.knockouts"
+                :min="range.knockouts[0]"
+                :max="range.knockouts[1]"
+                step="0"
+                thumb-label
+                thumb-size="24"
+                always-dirty
+                color="primary"
+                class="mt-4"
+              ></v-range-slider>
+            </th>
+
+            <th
+              :class="[
+                'column sortable',
+                pagination.descending ? 'desc' : 'asc',
+                'fitness' === pagination.sortBy ? 'active' : ''
+              ]"
+              @click="changeSort('fitness')"
+            >
+              <v-icon>arrow_upward</v-icon>
+              Fitness
+              <v-range-slider
+                @click.stop
+                v-model="filters.fitness"
+                :min="range.fitness[0]"
+                :max="range.fitness[1]"
+                step="0"
+                thumb-label
+                thumb-size="24"
+                always-dirty
+                color="primary"
+                class="mt-4"
+              ></v-range-slider>
+            </th>
+
+            <th
+              :class="[
+                'column sortable',
+                pagination.descending ? 'desc' : 'asc',
+                'yield' === pagination.sortBy ? 'active' : ''
+              ]"
+              @click="changeSort('yield')"
+            >
+              <v-icon>arrow_upward</v-icon>
+              Yield
+              <v-range-slider
+                @click.stop
+                v-model="filters.yield"
+                :min="range.yield[0]"
+                :max="range.yield[1]"
+                step="0"
+                thumb-label
+                thumb-size="24"
+                always-dirty
+                color="primary"
+                class="mt-4"
+              ></v-range-slider>
+            </th>
+
+            <th
+              :class="[
+                'column sortable',
+                pagination.descending ? 'desc' : 'asc',
+                'product' === pagination.sortBy ? 'active' : ''
+              ]"
+              @click="changeSort('product')"
+            >
+              <v-icon>arrow_upward</v-icon>
+              Production
+              <v-range-slider
+                @click.stop
+                v-model="filters.product"
+                :min="range.product[0]"
+                :max="range.product[1]"
+                step="0"
+                thumb-label
+                thumb-size="24"
+                always-dirty
+                color="primary"
+                class="mt-4"
+              ></v-range-slider>
+            </th>
+
+            <th
+              :class="[
+                'column sortable',
+                pagination.descending ? 'desc' : 'asc',
+                'biomass' === pagination.sortBy ? 'active' : ''
+              ]"
+              @click="changeSort('biomass')"
+            >
+              <v-icon>arrow_upward</v-icon>
+              Biomass
+              <v-range-slider
+                @click.stop
+                v-model="filters.biomass"
+                :min="range.biomass[0]"
+                :max="range.biomass[1]"
+                step="0"
+                thumb-label
+                thumb-size="24"
+                always-dirty
+                color="primary"
+                class="mt-4"
+              ></v-range-slider>
+            </th>
+
+            <th
+              :class="[
+                'column sortable',
+                pagination.descending ? 'desc' : 'asc',
+                'method' === pagination.sortBy ? 'active' : ''
+              ]"
+              @click="changeSort('method')"
+            >
+              <v-icon>arrow_upward</v-icon>
+              Method
+              <v-text-field
+                v-model="search"
+                append-icon="search"
+                single-line
+                hide-details
+              ></v-text-field>
             </th>
           </tr>
         </template>
@@ -94,10 +268,8 @@ export default Vue.extend({
   },
   data: () => ({
     selected: [],
+    search: "",
     expand: true,
-    showAllReactionKnockins: false,
-    showAllReactionKnockouts: false,
-    showAllGeneKnockouts: false,
     headers: [
       { text: "Manipulations", value: "manipulations" },
       { text: "Heterologous reactions", value: "heterologous_reactions" },
@@ -110,7 +282,9 @@ export default Vue.extend({
     ],
     pagination: {
       rowsPerPage: 25
-    }
+    },
+    range: null,
+    filters: null
   }),
   computed: {
     pathways() {
@@ -118,13 +292,140 @@ export default Vue.extend({
         ...this.prediction.result.cofactor_swap,
         ...this.prediction.result.diff_fva,
         ...this.prediction.result.opt_gene
-      ].map((item, index) => ({
-        id: index,
-        ...item
-      }));
-    }
+      ]
+        .filter(pathway => pathway.biomass > 0)
+        .map((pathway, index) => ({
+          id: index,
+          ...pathway
+        }));
+    },
+    filteredPathways() {
+      return this.pathways.filter(pathway => {
+        return Object.keys(this.filters).every(filter => {
+          if (
+            filter === "manipulations" ||
+            filter === "heterologous_reactions" ||
+            filter === "knockouts"
+          ) {
+            return (
+              pathway[filter].length >= this.filters[filter][0] &&
+              pathway[filter].length <= this.filters[filter][1]
+            );
+          }
+          return (
+            pathway[filter] >= this.filters[filter][0] &&
+            pathway[filter] <= this.filters[filter][1]
+          );
+        });
+      });
+    },
   },
   methods: {
+    getRange() {
+      return {
+        manipulations: [
+          this.pathways.reduce(
+            (min, pathway) =>
+              pathway.manipulations.length < min
+                ? pathway.manipulations.length
+                : min,
+            this.pathways[0].manipulations.length
+          ),
+          this.pathways.reduce(
+            (max, pathway) =>
+              pathway.manipulations.length > max
+                ? pathway.manipulations.length
+                : max,
+            this.pathways[0].manipulations.length
+          )
+        ],
+        heterologous_reactions: [
+          this.pathways.reduce(
+            (min, pathway) =>
+              pathway.heterologous_reactions.length < min
+                ? pathway.heterologous_reactions.length
+                : min,
+            this.pathways[0].heterologous_reactions.length
+          ),
+          this.pathways.reduce(
+            (max, pathway) =>
+              pathway.heterologous_reactions.length > max
+                ? pathway.heterologous_reactions.length
+                : max,
+            this.pathways[0].heterologous_reactions.length
+          )
+        ],
+        knockouts: [
+          this.pathways.reduce(
+            (min, pathway) =>
+              pathway.knockouts.length < min ? pathway.knockouts.length : min,
+            this.pathways[0].knockouts.length
+          ),
+          this.pathways.reduce(
+            (max, pathway) =>
+              pathway.knockouts.length > max ? pathway.knockouts.length : max,
+            this.pathways[0].knockouts.length
+          )
+        ],
+        fitness: [
+          this.pathways.reduce(
+            (min, pathway) => (pathway.fitness < min ? pathway.fitness : min),
+            this.pathways[0].fitness
+          ),
+          this.pathways.reduce(
+            (max, pathway) => (pathway.fitness > max ? pathway.fitness : max),
+            this.pathways[0].fitness
+          )
+        ],
+        yield: [
+          this.pathways.reduce(
+            (min, pathway) => (pathway.yield < min ? pathway.yield : min),
+            this.pathways[0].yield
+          ),
+          this.pathways.reduce(
+            (max, pathway) => (pathway.yield > max ? pathway.yield : max),
+            this.pathways[0].yield
+          )
+        ],
+        product: [
+          this.pathways.reduce(
+            (min, pathway) => (pathway.product < min ? pathway.product : min),
+            this.pathways[0].product
+          ),
+          this.pathways.reduce(
+            (max, pathway) => (pathway.product > max ? pathway.product : max),
+            this.pathways[0].product
+          )
+        ],
+        biomass: [
+          this.pathways.reduce(
+            (min, pathway) => (pathway.biomass < min ? pathway.biomass : min),
+            this.pathways[0].biomass
+          ),
+          this.pathways.reduce(
+            (max, pathway) => (pathway.biomass > max ? pathway.biomass : max),
+            this.pathways[0].biomass
+          )
+        ]
+      };
+    },
+    getFilters() {
+      return {
+        manipulations: [
+          this.range.manipulations[0],
+          this.range.manipulations[1]
+        ],
+        heterologous_reactions: [
+          this.range.heterologous_reactions[0],
+          this.range.heterologous_reactions[1]
+        ],
+        knockouts: [this.range.knockouts[0], this.range.knockouts[1]],
+        fitness: [this.range.fitness[0], this.range.fitness[1]],
+        yield: [this.range.yield[0], this.range.yield[1]],
+        product: [this.range.product[0], this.range.product[1]],
+        biomass: [this.range.biomass[0], this.range.biomass[1]]
+      };
+    },
     toggleAll() {
       if (this.selected.length) {
         this.selected = [];
@@ -140,6 +441,10 @@ export default Vue.extend({
         this.pagination.descending = false;
       }
     }
+  },
+  created() {
+    this.range = this.getRange();
+    this.filters = this.getFilters();
   }
 });
 </script>
