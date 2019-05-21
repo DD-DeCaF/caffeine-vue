227f7a38f176c8d9a265f8d13b29015c2ce277e1
Anna Sirunian <anna_sirunian@ukr.net>
2019-05-21 12:36:19 +0200

    feat: modify designs before storing

--- src/store/modules/designs.ts
+++ src/store/modules/designs.ts
@@ -14,7 +14,6 @@ export interface DesignItem {
     gene_knockouts: string[];
     reaction_knockins: string[];
     reaction_knockouts: string[];
-    added_reactions?: Object[];
   };
   id: number;
   model_id: number;
@@ -41,73 +40,18 @@ export default {
       axios
         .get(`${settings.apis.designStorage}/designs`)
         .then((response: AxiosResponse<DesignItem[]>) => {
-          const promises = response.data.map(design => {
-            // if design comes initially from job then full reaction information
-            // is already stored in the design storage
-            if (design.method === "Pathway") {
-              return {
-                ...design,
-                design: {
-                  ...design.design,
-                  added_reactions: design.design.reaction_knockins.map(
-                    reaction => JSON.parse(reaction)
-                  )
-                }
-              };
-            } else {
-              // otherwise request full information from BIGG
-              return axios
-                .all(
-                  design.design.reaction_knockins.map(reactionId =>
-                    axios.get(
-                      `${
-                        settings.apis.bigg
-                      }/models/universal/reactions/${reactionId}`
-                    )
-                  )
+          const designs = response.data.map(design => {
+            return {
+              ...design,
+              design: {
+                ...design.design,
+                reaction_knockins: design.design.reaction_knockins.map(
+                  reaction => JSON.parse(reaction)
                 )
-                .then(response => {
-                  let addedReactions: any = [];
-                  response.forEach(r => {
-                    const reaction = {
-                      id: r.data.bigg_id,
-                      name: r.data.name,
-                      reactionString: r.data.reaction_string,
-                      lowerBound: -1000,
-                      upperBound: 1000,
-                      gene_reaction_rule: "",
-                      metabolites: {
-                        ...r.data.metabolites.map(m => ({
-                          [`${m.bigg_id}_${
-                            m.compartment_bigg_id
-                          }`]: m.stoichiometrys
-                        }))
-                      },
-                      metabolites_to_add: r.data.metabolites.map(m => ({
-                        id: `${m.bigg_id}_${m.compartment_bigg_id}`,
-                        name: m.name,
-                        compartment: m.compartment_bigg_id,
-                        charge: m.stoichiometry
-                      }))
-                    };
-                    addedReactions.push(reaction);
-                  });
-                  return {
-                    ...design,
-                    design: {
-                      ...design.design,
-                      added_reactions: addedReactions
-                    }
-                  };
-                })
-                .catch(error => {
-                  commit("setFetchError", error, { root: true });
-                });
-            }
+              }
+            };
           });
-          Promise.all(promises).then(designs => {
-            commit("setDesigns", designs);
-          });
+          commit("setDesigns", designs);
         })
         .catch(error => {
           commit("setFetchError", error, { root: true });
