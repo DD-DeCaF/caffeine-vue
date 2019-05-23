b96e33230f195a1e0c7a0d75493e2c5ec1ce32ed
Ali Kaafarani <ali@kvikshaug.no>
2019-05-23 12:58:04 +0200

    feat: add loader when waiting for visualization

--- src/views/Jobs/JobResultsTable.vue
+++ src/views/Jobs/JobResultsTable.vue
@@ -25,11 +25,21 @@
               <v-btn
                 flat
                 color="primary"
-                :disabled="selected.length < 1"
+                :disabled="selected.length < 1 || isVisualizing"
                 class="mt-3"
                 @click="visualize()"
-                ><v-icon>share</v-icon>VISUALIZE</v-btn
               >
+                <v-icon v-if="!isVisualizing">share</v-icon>
+                <v-progress-circular
+                  v-else
+                  indeterminate
+                  color="primary"
+                  class="mr-1"
+                  :width="2"
+                  :size="15"
+                ></v-progress-circular>
+                Visualize
+              </v-btn>
             </v-list-tile>
           </v-list>
           <v-data-table
@@ -552,7 +562,10 @@ export default Vue.extend({
       down: "↓"
     },
     showAllManipulations: false,
-    showAllKnockouts: false
+    showAllKnockouts: false,
+    // True when user clicked visualize, and we're waiting mapping and/or model
+    // to load before routing them to the interactive map.
+    isVisualizing: false
   }),
   filters: {
     round: value => {
@@ -753,6 +766,7 @@ export default Vue.extend({
       }
     },
     visualize() {
+      this.isVisualizing = true;
       const predictions = this.selected.map(jobPrediction => {
         const addedReactionIds = [
           ...jobPrediction.heterologous_reactions,
