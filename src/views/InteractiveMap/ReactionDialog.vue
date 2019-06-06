4fd24ff1c6bc8dd9a360e2d4ef84d7e7aad23501
Anna Sirunian <anna_sirunian@ukr.net>
2019-06-06 10:48:54 +0200

    feat: add substrate part of a reaction

--- src/views/InteractiveMap/ReactionDialog.vue
+++ src/views/InteractiveMap/ReactionDialog.vue
@@ -1,7 +1,55 @@
 <template>
-  <v-dialog v-model="showDialog" width="700">
-    <v-card>
-      Dialog for adding a reaction
+  <v-dialog v-model="showDialog" width="1000">
+    <v-card class="pa-3">
+      <div class="body-2 mb-2">Reaction string:</div>
+      <v-text-field :value="reactionString" solo readonly></v-text-field>
+      <div>
+        <div class="body-1 ml-3 mb-2">
+          Substrates:
+        </div>
+        <v-layout column mx-3>
+          <div v-for="metabolite in substrates" :key="metabolite.id">
+            <v-layout>
+              <v-flex xs1>
+                <v-text-field
+                  v-model="metabolite.stoichiometry"
+                  solo
+                  class="mx-2"
+                  type="number"
+                ></v-text-field></v-flex
+              ><v-flex xs7>
+                <v-autocomplete
+                  v-model="metabolite.id"
+                  label="Metabolite"
+                  :items="metaboliteItems"
+                  :item-text="metaboliteDisplay"
+                  item-value="id"
+                  clearable
+                  class="mx-2"
+                >
+                </v-autocomplete
+              ></v-flex>
+              <v-flex xs3
+                ><v-autocomplete
+                  v-model="metabolite.compartment"
+                  label="Compartment"
+                  :items="compartmentItems"
+                  :item-text="compartmentDisplay"
+                  item-value="id"
+                  clearable
+                  class="mx-2"
+                >
+                </v-autocomplete
+              ></v-flex>
+              <v-flex xs1
+                ><v-btn icon @click="addSubstrate">
+                  <v-icon color="primary">add_circle</v-icon></v-btn
+                ></v-flex
+              >
+            </v-layout>
+          </div>
+        </v-layout>
+      </div>
     </v-card>
   </v-dialog>
 </template>
@@ -12,8 +60,37 @@ import Vue from "vue";
 export default Vue.extend({
   name: "ReactionDialog",
   props: ["model", "value"],
-  data: () => ({}),
+  data: () => ({
+    substrates: [
+      {
+        id: "",
+        name: "",
+        compartment: "",
+        stoichiometry: 1
+      }
+    ],
+    products: []
+  }),
   computed: {
+    reactionString() {
+      const substratesSerialized = this.serializeMetabolites(
+        this.substrates
+      ).join(" + ");
+      return substratesSerialized;
+    },
+    metaboliteItems() {
+      return this.model.model_serialized
+        ? this.model.model_serialized.metabolites
+        : [];
+    },
+    compartmentItems() {
+      return this.model.model_serialized
+        ? Object.keys(this.model.model_serialized.compartments).map(id => ({
+            id: id,
+            name: this.model.model_serialized.compartments[id]
+          }))
+        : [];
+    },
     showDialog: {
       get() {
         return this.value;
@@ -21,6 +98,37 @@ export default Vue.extend({
       set(value) {
         this.$emit("input", value);
       }
+    }
+  },
+  methods: {
+    metaboliteDisplay(metabolite) {
+      const id = metabolite.id.substring(0, metabolite.id.length - 2);
+      return `${metabolite.name} (${id})`;
+    },
+    compartmentDisplay(compartment) {
+      return `${compartment.name} (${compartment.id})`;
+    },
+    serializeMetabolites(metabolites) {
+      return metabolites.map(metabolite => {
+        const stoichiometryNormalized = Math.abs(metabolite.stoichiometry);
+        const stoichiometrySerialized =
+          stoichiometryNormalized === 1 ? "" : stoichiometryNormalized + " ";
+        const id = metabolite.id
+          ? metabolite.id.substring(0, metabolite.id.length - 2)
+          : "";
+        const compartment = metabolite.compartment
+          ? "_" + metabolite.compartment
+          : "";
+        return stoichiometrySerialized + id + compartment;
+      });
+    },
+    addSubstrate() {
+      this.substrates.push({
+        id: "",
+        name: "",
+        compartment: "",
+        stoichiometry: 1
+      });
     }
   }
 });
