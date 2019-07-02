3dc79a4931e6807f9ce3908d38269a96ab234d88
Ali Kaafarani <ali@kvikshaug.no>
2019-07-02 11:00:49 +0200

    style: require following curly brace conventions
    See https://eslint.org/docs/rules/curly

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/order-in-components": "error"
+    "vue/order-in-components": "error",
+    curly: "error"
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
