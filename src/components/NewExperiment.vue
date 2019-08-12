caccd687b54230cb813cacb33ca71ed50ce81159
Anna Sirunian <anna_sirunian@ukr.net>
2019-08-12 15:45:51 +0200

    feat: add experiments data table

---
+++ src/components/NewExperiment.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-dialog v-model="isDialogVisible">
+    <v-card class="text-xs-center pa-3">
+      Data upload tool
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "NewExperiment",
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    isDialogVisible: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  }
+});
+</script>
