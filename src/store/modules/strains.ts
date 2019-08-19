f74d5fbe3cb45e0d1542b41ae49b9e4898a6d5ac
Anna Sirunian <anna_sirunian@ukr.net>
2019-08-19 16:44:17 +0200

    feat: add strains to the store

---
+++ src/store/modules/strains.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { AxiosResponse } from "axios";
+import * as settings from "@/utils/settings";
+
+export interface StrainItem {
+  id: number;
+  name: string;
+  organism_id: number;
+  genotype: string;
+  parent_id: number;
+  project_id: number;
+  created: string;
+  updated: string;
+}
+
+export default {
+  namespaced: true,
+  state: {
+    strains: [],
+    strainsPromise: null
+  },
+  mutations: {
+    setStrains(state, strains: StrainItem[]) {
+      state.strains = strains;
+    },
+    addStrain(state, strain: StrainItem) {
+      state.strain.push(strain);
+    },
+    setStrainsPromise(state, strainsPromise) {
+      state.organismsPromise = strainsPromise;
+    }
+  },
+  actions: {
+    fetchStrains({ commit, dispatch }) {
+      const strainsPromise = axios
+        .get(`${settings.apis.warehouse}/strains`)
+        .then((response: AxiosResponse<StrainItem[]>) => {
+          commit("setStrains", response.data);
+        })
+        .catch(error => {
+          dispatch("setFetchError", error, { root: true });
+        });
+      commit("setStrainsPromise", strainsPromise);
+    }
+  }
+};
