f0906c0e5fc7c049d6231eac49d69fdf01848319
lukaw3d <lukaw3d@gmail.com>
2019-08-23 05:14:15 +0200

    feat: add compounds to media store

--- src/store/modules/media.ts
+++ src/store/modules/media.ts
@@ -13,24 +13,31 @@ export interface MediumItem {
   updated: string;
 }
 
-export interface MediumCompound {
+export interface Compound {
   id: number;
   name: string; // sodium chloride
   reference: string; // CHEBI:26710
   namespace_id: number; // CHEBI
   type_id: number; // compound
-  mass_concentration: number;
 
   project_id: number;
   created: string;
   updated: string;
 }
 
+export interface NewMediumCompound {
+  id: number;
+  mass_concentration: number;
+}
+
+export interface MediumCompound extends NewMediumCompound, Compound {}
+
 export default {
   namespaced: true,
   state: {
     media: [],
-    mediaPromise: null
+    mediaPromise: null,
+    _compoundsPromise: null
   },
   mutations: {
     setMedia(state, media: MediumItem[]) {
@@ -41,6 +48,9 @@ export default {
     },
     setMediaPromise(state, mediaPromise) {
       state.mediaPromise = mediaPromise;
+    },
+    setCompoundsPromise(state, compoundsPromise) {
+      state._compoundsPromise = compoundsPromise;
     }
   },
   actions: {
@@ -54,6 +64,27 @@ export default {
           dispatch("setFetchError", error, { root: true });
         });
       commit("setMediaPromise", mediaPromise);
+    },
+
+    /**
+     * Because getting all compounds takes a few seconds they are only fetched
+     * once - on first demand. To ensure correct usage, they should only be
+     * accessed through fetchCachedCompounds.
+     */
+    fetchCachedCompounds({ commit, dispatch, state }) {
+      if (state._compoundsPromise) {
+        return state._compoundsPromise;
+      }
+
+      const compoundsPromise = axios
+        .get<Compound[]>(`${settings.apis.warehouse}/bioentities/compounds`)
+        .then(response => response.data)
+        .catch(error => {
+          dispatch("setFetchError", error, { root: true });
+          throw error;
+        });
+      commit("setCompoundsPromise", compoundsPromise);
+      return compoundsPromise;
     }
   }
 };
