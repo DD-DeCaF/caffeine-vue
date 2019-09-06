6c8d9da605f7adabce4e2573ca3824591f333085
lukaw3d <lukaw3d@gmail.com>
2019-09-06 16:52:15 +0200

    feat: create metanetx metabolite autocomplete component

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ import FileUpload from "@/components/FileUpload.vue";
 import VSelectExtended from "@/components/VSelectExtended";
 import VAutocompleteExtended from "@/components/VAutocompleteExtended";
 import AutocompleteMnxReaction from "@/components/AutocompleteMnxReaction.vue";
+import AutocompleteMnxMetabolite from "@/components/AutocompleteMnxMetabolite.vue";
 import { initFromStorage } from "@/utils/startup";
 import * as Sentry from "@sentry/browser";
 import * as Integrations from "@sentry/integrations";
@@ -65,6 +66,7 @@ Vue.component("LoaderDialog", LoaderDialog);
 Vue.component("DeletionDialog", DeletionDialog);
 Vue.component("FileUpload", FileUpload);
 Vue.component("AutocompleteMnxReaction", AutocompleteMnxReaction);
+Vue.component("AutocompleteMnxMetabolite", AutocompleteMnxMetabolite);
 Vue.component("v-select-extended", VSelectExtended);
 Vue.component("v-autocomplete-extended", VAutocompleteExtended);
 
