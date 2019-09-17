dd0849c2522ae1eed5f8b1488f082d51194421db
Ali Kaafarani <ali@kvikshaug.no>
2019-09-17 13:34:12 +0200

    feat: add components to link to reactions/compounds based on namespace

---
+++ src/components/CompoundLink.vue
@@ -0,0 +1,33 @@
+<template>
+  <span>
+    <a v-if="link" :href="link" target="_blank">{{ identifier }}</a>
+    <span v-else>{{ identifier }} ({{ namespace }})</span>
+  </span>
+</template>
+
+<script lang="ts">
+/**
+ * For a given compound identifier + namespace, link to the relevant database if the
+ * namespace is recognized. Otherwise, simply print the identifier and namespace.
+ */
+
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "CompoundLink",
+  props: ["identifier", "namespace"],
+  computed: {
+    link() {
+      if (this.namespace === "bigg.metabolite") {
+        return `http://bigg.ucsd.edu/universal/metabolites/${this.identifier}`;
+      } else if (this.namespace === "CHEBI") {
+        return `https://www.ebi.ac.uk/chebi/searchId.do?chebiId=${
+          this.identifier
+        }`;
+      } else {
+        return null;
+      }
+    }
+  }
+});
+</script>
---
+++ src/components/ReactionLink.vue
@@ -0,0 +1,29 @@
+<template>
+  <span>
+    <a v-if="link" :href="link" target="_blank">{{ identifier }}</a>
+    <span v-else>{{ identifier }} ({{ namespace }})</span>
+  </span>
+</template>
+
+<script lang="ts">
+/**
+ * For a given reaction identifier + namespace, link to the relevant database if the
+ * namespace is recognized. Otherwise, simply print the identifier and namespace.
+ */
+
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "ReactionLink",
+  props: ["identifier", "namespace"],
+  computed: {
+    link() {
+      if (this.namespace === "bigg.reaction") {
+        return `http://bigg.ucsd.edu/universal/reactions/${this.identifier}`;
+      } else {
+        return null;
+      }
+    }
+  }
+});
+</script>
