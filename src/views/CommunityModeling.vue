0e57a8e1a12e3b6b934e360201bf8ae5199c8348
ChristianLieven <clie@biosustain.dtu.dk>
2020-01-27 16:08:06 +0100

    style: lint it routinely

--- src/views/CommunityModeling.vue
+++ src/views/CommunityModeling.vue
@@ -1,79 +1,83 @@
 <template>
-    <div class='interactive-map fill-height'>
+  <div class="interactive-map fill-height">
     <v-container>
-    <h1>Some test results go here</h1>
-    <v-btn
-      color="primary"
-      small
-      fab
-      top
-      right
-      class="sidepanel-toggle"
-      @click="isSidepanelOpen = true"
-    >
-      <v-icon>apps</v-icon>
-    </v-btn>
+      <h1>Some test results go here</h1>
+      <v-btn
+        color="primary"
+        small
+        fab
+        top
+        right
+        class="sidepanel-toggle"
+        @click="isSidepanelOpen = true"
+      >
+        <v-icon>apps</v-icon>
+      </v-btn>
     </v-container>
     <v-navigation-drawer
-    v-model="isSidepanelOpen"
-    right
-    absolute
-    hide-overlay
-    class="elevation-6"
+      v-model="isSidepanelOpen"
+      right
+      absolute
+      hide-overlay
+      class="elevation-6"
     >
-        <v-container class="py-1">
-            <v-select
-            :items='media'
-            :disabled="isUpdating"
-            label="Selected Medium"
-            autofocus
-            item-text="name"
-            item-value="id"
-            v-model="selectedMedium"
-            return-object
-            :rules="[v => !!v || 'Please choose a medium.']"
-            ></v-select>       
-             <v-autocomplete
-                v-model="selectedModels"
-                :disabled="isUpdating"
-                :items="models"
-                box
-                chips
-                label="Selected Models"
-                item-text="name"
-                item-value="id"
-                multiple
-                :rules="[v => v.length >= 2 || 'Please choose at least two metabolic models that you wish to simulate as a community.']"
-              >
-                <template v-slot:selection="data">
-                  <v-chip
-                    :selected="data.selected"
-                    close
-                    class="chip--select-multi"
-                    @input="remove(data.item)"
-                  >
-                    {{ data.item.name }}
-                  </v-chip>
-                </template>
-              </v-autocomplete>
-
-            <v-btn
-            :loading="isUpdating"
-            :disabled="isUpdating || !isValid"
-            color="primary"
-            depressed
-            @click="simulateCommunity"
+      <v-container class="py-1">
+        <v-select
+          :items="media"
+          :disabled="isUpdating"
+          label="Selected Medium"
+          autofocus
+          item-text="name"
+          item-value="id"
+          v-model="selectedMedium"
+          return-object
+          :rules="[v => !!v || 'Please choose a medium.']"
+        ></v-select>
+        <v-autocomplete
+          v-model="selectedModels"
+          :disabled="isUpdating"
+          :items="models"
+          box
+          chips
+          label="Selected Models"
+          item-text="name"
+          item-value="id"
+          multiple
+          :rules="[
+            v =>
+              v.length >= 2 ||
+              'Please choose at least two metabolic models that you wish to simulate as a community.'
+          ]"
+        >
+          <template v-slot:selection="data">
+            <v-chip
+              :selected="data.selected"
+              close
+              class="chip--select-multi"
+              @input="remove(data.item)"
             >
+              {{ data.item.name }}
+            </v-chip>
+          </template>
+        </v-autocomplete>
+
+        <v-btn
+          :loading="isUpdating"
+          :disabled="isUpdating || !isValid"
+          color="primary"
+          depressed
+          @click="simulateCommunity"
+        >
           <v-icon left>update</v-icon>
           Simulate Now
         </v-btn>
-        </v-container>
+      </v-container>
     </v-navigation-drawer>
     <v-snackbar color="error" v-model="hasSimulationError" :timeout="8000">
       Sorry, we were not able to complete the simulation successfully. Please
       try again in a few seconds, or contact us if the problem persists.
     </v-snackbar>
-    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -85,25 +89,56 @@ import { partitionedList } from "@/utils/utility";
 
 export default Vue.extend({
   name: "CommunityModeling",
-  components: {
-  },
+  components: {},
   data: () => ({
     isUpdating: false,
-    selectedModels:[],
+    selectedModels: [],
     selectedMedium: null,
     isSidepanelOpen: true,
     hasSimulationError: false,
     communityData: null,
-    media: [{'id':1, 'name':"M9 Glucose", 'componentIDs':["ca2", "cl", "co2", "cobalt2", "cu2", "fe2", "glc__D", "h2o", "h", "hco3", "k", "mg2", "mn2", "mobd", "na1", "nh4", "ni2", "o2", "pi", "sel", "so4", "tungs", "zn2"]}]
+    media: [
+      {
+        id: 1,
+        name: "M9 Glucose",
+        componentIDs: [
+          "ca2",
+          "cl",
+          "co2",
+          "cobalt2",
+          "cu2",
+          "fe2",
+          "glc__D",
+          "h2o",
+          "h",
+          "hco3",
+          "k",
+          "mg2",
+          "mn2",
+          "mobd",
+          "na1",
+          "nh4",
+          "ni2",
+          "o2",
+          "pi",
+          "sel",
+          "so4",
+          "tungs",
+          "zn2"
+        ]
+      }
+    ]
   }),
   computed: {
     isValid() {
-        if (this.selectedModels.length >= 2 && this.selectedMedium !== null) {
+      if (this.selectedModels.length >= 2 && this.selectedMedium !== null) {
         return true;
-        } else { return false;}
+      } else {
+        return false;
+      }
     },
     models() {
-    return partitionedList("models", "organisms");
+      return partitionedList("models", "organisms");
     },
     availableModels() {
       return this.$store.state.models.models;
@@ -112,16 +147,15 @@ export default Vue.extend({
       organism: "organisms/getOrganismById"
     })
   },
-  mounted() {
-  },
+  mounted() {},
   methods: {
     simulateCommunity() {
-        this.isUpdating = true;
-        const payload = {
-            medium: this.selectedMedium.componentIDs,
-            model_ids: this.selectedModels
-        };
-        axios
+      this.isUpdating = true;
+      const payload = {
+        medium: this.selectedMedium.componentIDs,
+        model_ids: this.selectedModels
+      };
+      axios
         .post(`${settings.apis.simulations}/community/simulate`, payload)
         .then(response => {
           this.communityData = response.data;
@@ -133,9 +167,11 @@ export default Vue.extend({
           this.hasSimulationError = true;
         });
     },
-    remove (item) {
-    const index = this.selectedModels.indexOf(item.id)
-    if (index >= 0) { this.selectedModels.splice(index, 1) }
+    remove(item) {
+      const index = this.selectedModels.indexOf(item.id);
+      if (index >= 0) {
+        this.selectedModels.splice(index, 1);
+      }
     }
   }
 });
@@ -146,4 +182,3 @@ export default Vue.extend({
   position: absolute;
 }
 </style>
-
