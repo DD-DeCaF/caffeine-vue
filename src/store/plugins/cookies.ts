13ae4ee2091b2c1ce2bf7feafee9f158123f0129
Juraj Oravec <juraj.oravec.josefson@gmail.com>
2020-02-05 11:24:25 +0100

    fix: cookies policy logs user out despite consent

--- src/store/plugins/cookies.ts
+++ src/store/plugins/cookies.ts
@@ -9,26 +9,45 @@ import { Store } from "vuex";
  * on beforeunload.
  */
 export const cookiePlugin = (store: Store<any>) => {
+  const clearPreferencesCookies = () => {
+    // Do nothing if consents are not enabled
+    if (!store.state.consents.enableConsents) {
+      return;
+    }
+    // Sanity check - do nothing if preferences cookies accepted
+    if (
+      store.getters["consents/isConsentAccepted"]({
+        type: "cookie",
+        category: "preferences"
+      })
+    ) {
+      return;
+    }
+    ["jwt", "cookie:accepted", "consents"].forEach(k =>
+      localStorage.removeItem(k)
+    );
+  };
+  // Guard against assigning multiple on-event callbacks
+  let clearPrefFuncAssigned = false;
+
   store.subscribe(({ type, payload }, state) => {
-    const clearPreferencesCookies = () => {
-      // Do nothing if consents are not enabled
-      if (!store.state.consents.enableConsents) {
-        return;
-      }
-      ["jwt", "cookie:accepted", "consents"].forEach(k =>
-        localStorage.removeItem(k)
-      );
-    };
     if (type === "consents/setConsent") {
       if (payload.type === "cookie" && payload.category === "preferences") {
         if (store.getters["consents/isConsentAccepted"](payload)) {
           window.removeEventListener("beforeunload", clearPreferencesCookies);
+          clearPrefFuncAssigned = false;
         } else {
-          window.addEventListener("beforeunload", clearPreferencesCookies);
+          if (!clearPrefFuncAssigned) {
+            window.addEventListener("beforeunload", clearPreferencesCookies);
+            clearPrefFuncAssigned = true;
+          }
         }
       }
     } else if (type === "consents/clearConsents") {
-      window.addEventListener("beforeunload", clearPreferencesCookies);
+      if (!clearPrefFuncAssigned) {
+        window.addEventListener("beforeunload", clearPreferencesCookies);
+        clearPrefFuncAssigned = true;
+      }
     }
   });
 };
