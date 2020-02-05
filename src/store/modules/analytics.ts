fdd54dfdcee6e90e9c5409fa1c455f5d0523b60c
Juraj Oravec <juraj.oravec.josefson@gmail.com>
2020-02-05 12:07:32 +0100

    feat: track logging in for from localStorage

--- src/store/modules/analytics.ts
+++ src/store/modules/analytics.ts
@@ -40,6 +40,22 @@ export default vuexStoreModule({
     login({ state }, payload) {
       state.analytics!.track("login", payload);
     },
+    loginLocalStorage({ dispatch, rootState }) {
+      // Make an API call to get user's data to handle the edge case of when
+      // the user has already been logged in before web analytics were set up
+      let unsubscribe = store.subscribe((mutation, state) => {
+        if (!(rootState as RootState).session.isAuthenticated) {
+          return;
+        }
+        axios
+          .get(`${settings.apis.iam}/user`)
+          .then((response: AxiosResponse<AIMUser>) => {
+            dispatch("login", response.data);
+          })
+          .catch(error => null);
+        unsubscribe();
+      });
+    },
     identifyUser({ state }, payload) {
       state.analytics!.identify(payload.registeredEmail, {
         ...payload,
